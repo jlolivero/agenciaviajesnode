66fda38fdc36d43975b49d0e12c78bcdba9022ee
jlolivero <166930917+jlolivero@users.noreply.github.com>
2024-04-14 02:05:58 +0000

    ACTUALIZACION DE PUERTO EN DB

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ dotenv.config()
 
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
-    port: '58650',
+    port: process.env.DB_PORT,
     dialect: 'mysql',
     define: {
         timestamps: false
